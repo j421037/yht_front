bdee50ebd608c848155128b4b8f922910c0f1cca
WangXin <wangxin@WangXindeiMac.local>
2019-05-14 01:27:35 +0800

    2019-05-14 01:30

--- src/util/appConst.js
+++ src/util/appConst.js
@@ -1,4 +1,4 @@
-let BASEURL = "http://dev.miaoyi09.com/back/public/index.php/";
+let BASEURL = "http://k.yhtjc.com/public/index.php/";
 
 if (process.env.NODE_ENV === 'development') {
     //BASEURL= "http://192.168.0.250/yhtsys/public/index.php/";
